1c04eb9ff863a04a19985ed8559621685a7f2087
brn.developers <brn.developers@achiever-108.local>
2018-05-14 16:47:12 +0530

    printf

---
+++ Github/Github/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Github
+//
+//  Created by brn.developers on 5/14/18.
+//  Copyright © 2018 brn.developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
